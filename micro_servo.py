333d9f80b2f762099ec59e93f323a765407380f6
The1stAtomic <your-email@gmail.com>
2026-02-06 17:16:16 +0700

    6.2

--- micro_servo.py
+++ micro_servo.py
@@ -11,7 +11,7 @@ BASE_URL = "https://attcam.cc/api/devices"
 
 # --- 2. HARDWARE SETUP ---
 # Servo on GPIO 18
-servo = AngularServo(18, min_angle=-90, max_angle=90, min_pulse_width=0.0005, max_pulse_width=0.0025)
+servo = AngularServo(18, min_angle=90, max_angle=-90, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 # Button on Pin 36 (GPIO 16)
 door_button = Button(16) 
